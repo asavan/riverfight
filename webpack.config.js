cc8bc278b356dd5023594c3d2eb6d6a433390448
a.savanovich <asavanster@gmail.com>
2020-07-02 22:53:18 +0300

    install button fix

--- webpack.config.js
+++ webpack.config.js
@@ -9,31 +9,13 @@ const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 const webpack = require('webpack');
 const {GenerateSW} = require('workbox-webpack-plugin');
 
+const getLocalExternalIP = () => [].concat(...Object.values(os.networkInterfaces()))
+    .filter(details => details.family === 'IPv4' && !details.internal)
+    .pop().address
 
 module.exports = (env, argv) => {
     const devMode = (argv.mode !== 'production');
-    const ifaces = os.networkInterfaces();
-    let addr = '0.0.0.0';
-    Object.keys(ifaces).forEach(function (ifname) {
-        let alias = 0;
-
-        ifaces[ifname].forEach(function (iface) {
-            if ('IPv4' !== iface.family || iface.internal !== false) {
-                // skip over internal (i.e. 127.0.0.1) and non-ipv4 addresses
-                return;
-            }
-
-            if (alias >= 1) {
-                // this single interface has multiple ipv4 addresses
-                console.log(ifname + ':' + alias, iface.address);
-            } else {
-                // this interface has only one ipv4 adress
-                console.log(ifname, iface.address);
-                addr = iface.address;
-            }
-            ++alias;
-        });
-    });
+    let addr = getLocalExternalIP() || '0.0.0.0';
     return {
 
         entry: {main: "./src/index.js"},
