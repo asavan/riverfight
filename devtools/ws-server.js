c01978da39f6339c9812271c05e1bd220814a359
a.savanovich <asavanster@gmail.com>
2025-10-07 10:44:28 +0300

    move to netdeps

--- devtools/ws-server.js
+++ devtools/ws-server.js
@@ -1,22 +1,3 @@
-import { WebSocketServer, WebSocket } from "ws";
 import settings from "../src/js/settings.js";
-const wss = new WebSocketServer({port: settings.wsPort});
-
-wss.on("connection", (ws, req) => {
-    console.log("WS connection established!", req.socket.remoteAddress, req.headers['sec-websocket-key']);
-
-    ws.on("close", function () {
-        console.log("WS closed!");
-    });
-
-    ws.on("message", function (message) {
-        console.log("Got ws message: " + message);
-        for (const candidate of wss.clients) {
-            // send to everybody on the site, except sender
-            if (candidate !== ws && candidate.readyState === WebSocket.OPEN) {
-                // console.log("Send ws message: " + message);
-                candidate.send(message);
-            }
-        }
-    });
-});
+import {wsServer} from "netdeps";
+wsServer(settings.wsPort);
